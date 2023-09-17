5bcf6af5ef1225a748e788ee56305e596c2df3dd
Omar Jibirl Abdullkadir <129175570+Engr-Pirlo@users.noreply.github.com>
2023-09-17 11:50:27 +0300

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className="tex-4xl bg-pink-600 flex items-center justify-center">User Authentication</h1>
+  )
+}
+
+export default App
